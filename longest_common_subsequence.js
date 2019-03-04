9eb8221558b4423a6d4440459fbaf2e32be77c9e
Husni Munaya <husnimun@gmail.com>
2019-03-04 16:57:00 +0700

    Update longest_common_subsequence.js
    Refactor

--- longest_common_subsequence.js
+++ longest_common_subsequence.js
@@ -10,20 +10,23 @@ function lcs_rec(s1, s2, i1, i2, memo) {
 
   let key = `${i1}-${i2}`
   if (memo[key]) {
-    console.log('use memo')
+    return memo[key]
   }
 
   if (s1[i1] === s2[i2]) {
     let result = s1[i1] + lcs_rec(s1, s2, i1 + 1, i2 + 1, memo)
-    memo[`${i1 + 1}-${i2 + 1}`]
+    let key = `${i1 + 1}-${i2 + 1}`
+    memo[key]
     return result
   }
 
   let resultA = lcs_rec(s1, s2, i1 + 1, i2, memo)
   let resultB = lcs_rec(s1, s2, i1, i2 + 1, memo)
-
-  memo[`${i1 + 1}-${i2}`] = resultA
-  memo[`${i1}-${i2 + 1}`] = resultB
+  
+  let keyA = `${i1 + 1}-${i2}`
+  let keyB = `${i1}-${i2 + 1}`
+  memo[keyA] = resultA
+  memo[keyB] = resultB
 
   return resultA.length > resultB.length ? resultA : resultB
 }
